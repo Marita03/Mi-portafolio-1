500aae435892babaa6c984006bba27438a22e084
María Ignacia <Miorellanam@uc.cl>
2025-11-01 22:05:40 -0300

    p1

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const portfolio = document.querySelector("#popote");
+
+async function datos(raw) {
+    try {
+        let consulta = await fetch(raw);
+let resultado = await consulta.json();
+let trabajos = resultado.data;
+console.log(trabajos);
+        trabajos.forEach((trabajo) => {
+            portfolio.innerHTML += `
+
+                            <div class="col">
+                                <div class="card shadow-sm">
+                                <img src="${trabajo.photo}" class="card-img-top">
+                                <div class="card-body">
+                                <p class="card-text">${trabajo.title}</p>
+                                <p class="card-text">${trabajo.description}</p>
+                                <div class="d-flex
+                                justify-content-between
+                                align-items-center">
+                                <div class="btn-group">
+                                <button type="button" class="btn btn btn-outline-secondary">${trabajo.category}</button>
+                                </div>
+                                <small class="text-body-secondary">Reciente</small>
+             </div>
+             </div>
+            </div>
+            </div>`;
+        });
+    } catch (error) {
+        console.error("Error al cargar los datos:", error);
+    }
+}
+
+datos("https://api.myjson.online/v1/records/72a0a513-be36-4a6c-b360-812151ac4e75");
